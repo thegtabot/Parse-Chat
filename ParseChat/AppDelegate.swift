e9d6f6c7dac965a73743c9a0fe4ffad232430d8f
thegtabot <gtas321@gmail.com>
2018-10-22 16:51:32 -0400

    Update AppDelegate.swift

--- ParseChat/AppDelegate.swift
+++ ParseChat/AppDelegate.swift
@@ -1,10 +1,4 @@
-//
-//  AppDelegate.swift
-//  ParseChat
-//
-//  Created by Avishek Thapa Magar on 10/4/18.
-//  Copyright © 2018 Avishek Thapa Magar. All rights reserved.
-//
+
 
 import UIKit
 import Parse
